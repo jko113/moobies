3c99f25a0310435e03524b396648c743ff0df977
Joshua Owens <jko113@gmail.com>
2018-06-19 15:51:08 -0400

    couching in if statement

--- index.js
+++ index.js
@@ -25,67 +25,70 @@ app.get("/", (req, res) => {
 });
 
 app.get("/:id", (req, res) => {
-    let movieId = req.params.id;
-    console.log(req.params.id);
+    if (req.params.id !== "favicon.ico") {
 
-    let requestUrl = omdbOrigin + movieId + apiKey;
-
-    http.get(requestUrl, (apiRes) => {
-
-        let unparsedData = "";
-        apiRes.on("data", (chunk) => {
-            unparsedData += chunk;
-        }).on("end", () => {
-            data = JSON.parse(unparsedData);
-            // let isFalse = Object.values(data)[13] +
-            // Object.values(data)[14] +
-            // Object.values(data)[15] +
-            // Object.values(data)[16] +
-            // Object.values(data)[17];
-            //console.log(Object.values(data)[13]);
-            if (data.Response === "False") {
-                console.log(requestUrl);
-                console.log(data);
-                throw new Error("An error occurred fetching the data.");
-            }
-            // res.send(JSON.parse(data));
-            // data = JSON.parse(data);
-            // res.send(Object.keys(data));
-
-            res.render("moobie", {
-                // fullData: JSON.stringify(data),
-                fullData: unparsedData,
-                entries: Object.entries(data),
-                title: data.Title,
-                year: data.Year,
-                rated: data.Rated,
-                released: data.Released,
-                runtime: data.Runtime,
-                genre: data.Genre,
-                director: data.Director,
-                actors: data.Actors,
-                writers: data.Writer,
-                // ratings: data.Ratings
-                plot: data.Plot,
-                language: data.Language,
-                country: data.Country,
-                awards: data.Awards,
-                poster: data.Poster,
-                metascore: data.Metascore,
-                imdbRating: data.imdbRating,
-                imdbVotes: data.imdbVotes,
-                //imdbID: id,
-                type: data.Type,
-                DVD: data.DVD,
-                boxOffice: data.BoxOffice,
-                production: data.Production,
-                website: data.Website
-
-                // data: JSON.stringify(data)
+        let movieId = req.params.id;
+        console.log(req.params.id);
+    
+        let requestUrl = omdbOrigin + movieId + apiKey;
+    
+        http.get(requestUrl, (apiRes) => {
+    
+            let unparsedData = "";
+            apiRes.on("data", (chunk) => {
+                unparsedData += chunk;
+            }).on("end", () => {
+                data = JSON.parse(unparsedData);
+                // let isFalse = Object.values(data)[13] +
+                // Object.values(data)[14] +
+                // Object.values(data)[15] +
+                // Object.values(data)[16] +
+                // Object.values(data)[17];
+                //console.log(Object.values(data)[13]);
+                if (data.Response === "False") {
+                    console.log(requestUrl);
+                    console.log(data);
+                    throw new Error("An error occurred fetching the data.");
+                }
+                // res.send(JSON.parse(data));
+                // data = JSON.parse(data);
+                // res.send(Object.keys(data));
+    
+                res.render("moobie", {
+                    // fullData: JSON.stringify(data),
+                    fullData: unparsedData,
+                    entries: Object.entries(data),
+                    title: data.Title,
+                    year: data.Year,
+                    rated: data.Rated,
+                    released: data.Released,
+                    runtime: data.Runtime,
+                    genre: data.Genre,
+                    director: data.Director,
+                    actors: data.Actors,
+                    writers: data.Writer,
+                    // ratings: data.Ratings
+                    plot: data.Plot,
+                    language: data.Language,
+                    country: data.Country,
+                    awards: data.Awards,
+                    poster: data.Poster,
+                    metascore: data.Metascore,
+                    imdbRating: data.imdbRating,
+                    imdbVotes: data.imdbVotes,
+                    //imdbID: id,
+                    type: data.Type,
+                    DVD: data.DVD,
+                    boxOffice: data.BoxOffice,
+                    production: data.Production,
+                    website: data.Website
+    
+                    // data: JSON.stringify(data)
+                });
             });
+    
         });
-
-    });
+    }
 
 });
 
